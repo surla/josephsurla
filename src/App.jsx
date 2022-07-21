935434b908644927c8700a2f3f7cf12d0b70cb6b
Joseph Surla <josephsurla@gmail.com>
2022-07-21 12:28:20 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold center">Joseph Surla</h1>
+      <h3>Web Developer</h3>
+    </div>
+  );
+}
+
+export default App;
